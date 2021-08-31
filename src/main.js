9cb046700c87a0389238fb502908927118a522c5
Andrew Card <mrsaltinecracker1@gmail.com>
2021-08-31 01:21:57 -0700

    Should Be Ready For Use

--- src/main.js
+++ src/main.js
@@ -95,11 +95,8 @@ function createWindow() {
     });
     // Create the browser window.
     mainWindow = new BrowserWindow({
-        width: 1920,
-        height: 816,
-        // 1366x570 is a good standard height, but you may want to change this to fit your DriverStation's screen better.
-        // It's best if the dashboard takes up as much space as possible without covering the DriverStation application.
-        // The window is closed until the python server is ready
+        width: 1166,
+        height: 718,
         show: false,
         icon: __dirname + '/../images/icon.png',
         webPreferences: {
