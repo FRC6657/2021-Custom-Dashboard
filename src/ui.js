1515408a0c3bee30ad3b28609cfd733e1726aef5
Andrew Card <mrsaltinecracker1@gmail.com>
2021-09-19 16:24:44 -0700

    Condensed setPipeLine function

--- src/ui.js
+++ src/ui.js
@@ -18,73 +18,22 @@ let ui = {
 
 };
 
-//This is probably horribly inefficient but ask me if I care
+/**
+ * Author Scott Wegley
+ */
 function setPipeline(pipeline){
 
-    document.getElementById("pipe0").style.color.white;
-    document.getElementById("pipe1").style.color.white;
-    document.getElementById("pipe2").style.color.white;
-    document.getElementById("pipe3").style.color.white;
-    document.getElementById("pipe4").style.color.white;
-    document.getElementById("pipe5").style.color.white;
-    document.getElementById("pipe6").style.color.white;
-    document.getElementById("pipe7").style.color.white;
-    document.getElementById("pipe8").style.color.white;
-
-    document.getElementById("pipe0").style.backgroundColor = '#101213'
-    document.getElementById("pipe1").style.backgroundColor = '#101213'
-    document.getElementById("pipe2").style.backgroundColor = '#101213'
-    document.getElementById("pipe3").style.backgroundColor = '#101213'
-    document.getElementById("pipe4").style.backgroundColor = '#101213'
-    document.getElementById("pipe5").style.backgroundColor = '#101213'
-    document.getElementById("pipe6").style.backgroundColor = '#101213'
-    document.getElementById("pipe7").style.backgroundColor = '#101213'
-    document.getElementById("pipe8").style.backgroundColor = '#101213'
-    document.getElementById("pipe9").style.backgroundColor = '#101213'
-    
-    switch(pipeline){
-        case '0':
-            document.getElementById("pipe0").style.color.black;
-            document.getElementById("pipe0").style.backgroundColor = '#50940A'
-            break;
-        case '1':
-            document.getElementById("pipe1").style.color.black;
-            document.getElementById("pipe1").style.backgroundColor = '#50940A'
-            break;
-        case '2':
-            document.getElementById("pipe2").style.color.black;
-            document.getElementById("pipe2").style.backgroundColor = '#50940A'
-            break;
-        case '3':
-            document.getElementById("pipe3").style.color.black;
-            document.getElementById("pipe3").style.backgroundColor = '#50940A'
-            break;
-        case '4':
-            document.getElementById("pipe4").style.color.black;
-            document.getElementById("pipe4").style.backgroundColor = '#50940A'
-            break;
-        case '5':
-            document.getElementById("pipe5").style.color.black;
-            document.getElementById("pipe5").style.backgroundColor = '#50940A'
-            break;
-        case '6':
-            document.getElementById("pipe6").style.color.black;
-            document.getElementById("pipe6").style.backgroundColor = '#50940A'
-            break;
-        case '7':
-            document.getElementById("pipe7").style.color.black;
-            document.getElementById("pipe7").style.backgroundColor = '#50940A'
-            break;
-        case '8':
-            document.getElementById("pipe8").style.color.black;
-            document.getElementById("pipe8").style.backgroundColor = '#50940A'
-            break;
-        case '9':
-            document.getElementById("pipe9").style.color.black;
-            document.getElementById("pipe9").style.backgroundColor = '#50940A'
-            break;
+    for (let i = 0; i < 10; i++){
+            var myString = "pipe" + i.toString();
+            document.getElementById(myString).style.color.white;
+            document.getElementById(myString).style.backgroundColor = '#101213';
     }
-    NetworkTables.putValue('/limelight/pipeline', pipeline)
+    
+    document.getElementById("pipe" + pipeline.toString()).style.color.black;
+    document.getElementById("pipe" + pipeline.toString()).style.backgroundColor = '#50940A';
+    
+    NetworkTables.putValue('/limelight/pipeline', pipeline);
+    
 }
 
 function lightState(){
